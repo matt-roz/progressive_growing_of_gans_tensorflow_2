abe458495794988bce723d81993cf94f1d86215f
Matthias Rozanski <matthias.rozanski@rwth-aachen.de>
2020-04-05 11:00:26 -0700

    refactored tf.functions and train loop

--- train.py
+++ train.py
@@ -14,6 +14,7 @@ def get_dataset_pipeline(
         name: str,
         split: str,
         data_dir: Union[str, os.PathLike],
+        as_supervised: bool = False,
         batch_size: Optional[int] = None,
         buffer_size: Optional[int] = None,
         process_func: Optional[Callable] = None,
@@ -71,7 +72,7 @@ def train(arguments):
         if arguments.is_chief:
             logging.info(f"{arguments.host}: train_dataset contains {num_examples} images with shape={image_shape}")
 
-        # instantiate optimizers and loss
+        # instantiate optimizers
         optimizer_gen = tf.keras.optimizers.Adam(
             learning_rate=arguments.learningrate,
             beta_1=arguments.beta1,
@@ -86,7 +87,7 @@ def train(arguments):
             epsilon=arguments.epsilon,
             name='adam_discriminator'
         )
-        loss = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+        tf_loss_obj = tf.keras.losses.BinaryCrossentropy(from_logits=True)
 
         # get model
         model_gen = celeb_a_generator(input_tensor=None, input_shape=(arguments.noisedim,))
@@ -95,119 +96,88 @@ def train(arguments):
             model_gen.summary(print_fn=logging.info, line_length=170, positions=[.33, .55, .67, 1.])
             model_dis.summary(print_fn=logging.info, line_length=170, positions=[.33, .55, .67, 1.])
 
-        # epoch iterable, chief iterates over tqdm for status prints
-        epochs = range(arguments.epochs)
-        if arguments.is_chief:
-            epochs = tqdm(iterable=epochs, desc='Progressive-GAN', unit='epoch')
+    # local tf.function definitions for fast graphmode execution
+    @tf.function
+    def discriminator_loss(real_output, fake_output):
+        real_loss = tf_loss_obj(tf.ones_like(real_output), real_output)
+        fake_loss = tf_loss_obj(tf.zeros_like(fake_output), fake_output)
+        total_loss = real_loss + fake_loss
+        return total_loss
 
-        # train loop
-        for epoch in epochs:
-            epoch_start_time = time.time()
-            gen_loss, dis_loss, image_count = epoch_step(
-                generator=model_gen,
-                discriminator=model_dis,
-                optimizer_generator=optimizer_gen,
-                optimizer_discriminator=optimizer_dis,
-                dataset=train_dataset,
-                tf_loss=loss,
-                noise_dim=arguments.noisedim,
-                epoch=epoch,
-                steps_per_epoch=int(num_examples // arguments.globalbatchsize) + 1,
-                is_chief=arguments.is_chief
-            )
-            epoch_duration = time.time() - epoch_start_time
+    @tf.function
+    def generator_loss(fake_output):
+        return tf_loss_obj(tf.ones_like(fake_output), fake_output)
+
+    @tf.function
+    def train_step(image_batch, local_batch_size):
+        # generate noise for projecting fake images
+        noise = tf.random.normal([local_batch_size, arguments.noisedim])
+
+        # forward pass: inference through both models on tape, compute losses
+        with tf.GradientTape() as generator_tape, tf.GradientTape() as discriminator_tape:
+            fake_images = model_gen(noise, training=True)
+
+            real_image_guesses = model_dis(image_batch, training=True)
+            fake_image_guesses = model_dis(fake_images, training=True)
+
+            _gen_loss = generator_loss(fake_image_guesses)
+            _disc_loss = discriminator_loss(real_image_guesses, fake_image_guesses)
+
+        # collocate gradients from tapes
+        gradients_generator = generator_tape.gradient(_gen_loss, model_gen.trainable_variables)
+        gradients_discriminator = discriminator_tape.gradient(_disc_loss, model_dis.trainable_variables)
+        # backward pass: apply gradients via optimizers to update models
+        optimizer_gen.apply_gradients(zip(gradients_generator, model_gen.trainable_variables))
+        optimizer_dis.apply_gradients(zip(gradients_discriminator, model_dis.trainable_variables))
+        return _gen_loss, _disc_loss
+
+    def epoch_step(dataset, num_epoch, num_steps):
+        # return metrics
+        _epoch_gen_loss, _epoch_dis_loss, _image_count = 0.0, 0.0, 0.0
+
+        # epoch iterable, chief iterates over tqdm for status prints - all other workers over tf.data.Dataset
+        dataset = tqdm(iterable=dataset, desc=f"epoch-{num_epoch + 1:04d}", unit="batch", total=num_steps, leave=False)
+
+        for image_batch in dataset:
+            batch_size = tf.shape(image_batch)[0]
+            batch_gen_loss, batch_dis_loss = train_step(image_batch=image_batch, local_batch_size=batch_size)
+
+            # compute moving average of loss metrics
+            _size = tf.cast(batch_size, tf.float32)
+            _epoch_gen_loss = (_image_count * _epoch_gen_loss + _size * batch_gen_loss) / (_image_count + _size)
+            _epoch_dis_loss = (_image_count * _epoch_dis_loss + _size * batch_dis_loss) / (_image_count + _size)
+            _image_count += _size
 
             # additional chief tasks during training
-            if arguments.is_chief:
-                # save eval images
-                if arguments.evaluate and arguments.evalfrequency and (epoch + 1) % arguments.evalfrequency == 0:
-                    pass
-                # save model checkpoints
-                if arguments.saving and arguments.checkpointfrequency and (epoch + 1) % arguments.checkpointfrequency == 0:
-                    pass
-                # update log files and tqdm status message
-                status_message = f"sec={epoch_duration:.3f}, gen_loss={gen_loss:.3f}, dis_loss={dis_loss:.3f}"
-                logging.info(f"finished epoch-{epoch+1:04d} with {status_message}")
-                epochs.set_postfix_str(status_message)
+            batch_status_message = f"batch_gen_loss={batch_gen_loss:.3f}, batch_dis_loss={batch_dis_loss:.3f}"
+            dataset.set_postfix_str(batch_status_message)
+            logging.debug(batch_status_message)
 
+        return _epoch_gen_loss, _epoch_dis_loss, tf.cast(_image_count, tf.int32)
 
-def epoch_step(
-        generator: tf.keras.Model,
-        discriminator: tf.keras.Model,
-        optimizer_generator: tf.keras.optimizers.Optimizer,
-        optimizer_discriminator: tf.keras.optimizers.Optimizer,
-        dataset: tf.data.Dataset,
-        tf_loss: tf.keras.losses.Loss,
-        noise_dim: int,
-        epoch: int,
-        steps_per_epoch: int,
-        is_chief: bool) -> Tuple[float, float, int]:
-    # return metrics
-    epoch_gen_loss, epoch_dis_loss, image_count = 0.0, 0.0, 0
+    def train_loop():
+        epochs = tqdm(iterable=range(arguments.epochs), desc='Progressive-GAN', unit='epoch')
+        steps_per_epoch = int(num_examples // arguments.globalbatchsize) + 1
 
-    # epoch iterable, chief iterates over tqdm for status prints - all other workers over tf.data.Dataset
-    if is_chief:
-        dataset = tqdm(iterable=dataset, desc=f"epoch-{epoch+1:04d}", unit="batch", total=steps_per_epoch, leave=False)
+        for epoch in epochs:
+            epoch_start_time = time.time()
+            gen_loss, dis_loss, image_count = epoch_step(train_dataset, epoch, steps_per_epoch)
+            epoch_duration = time.time() - epoch_start_time
 
-    for image_batch in dataset:
-        batch_size = len(image_batch)
-        batch_gen_loss, batch_dis_loss = train_step(
-            generator=generator,
-            discriminator=discriminator,
-            optimizer_generator=optimizer_generator,
-            optimizer_discriminator=optimizer_discriminator,
-            image_batch=image_batch,
-            noise_dim=noise_dim,
-            tf_loss=tf_loss,
-        )
+            # save eval images
+            if arguments.evaluate and arguments.evalfrequency and (epoch + 1) % arguments.evalfrequency == 0:
+                pass
 
-        # compute moving average of loss metrics
-        epoch_gen_loss = (image_count * epoch_gen_loss + batch_size * batch_gen_loss) / (image_count + batch_size)
-        epoch_dis_loss = (image_count * epoch_dis_loss + batch_size * batch_dis_loss) / (image_count + batch_size)
-        image_count += batch_size
+            # save model checkpoints
+            if arguments.saving and arguments.checkpointfrequency and (epoch + 1) % arguments.checkpointfrequency == 0:
+                str_image_shape = 'x'.join([str(x) for x in image_shape])
+                model_gen.save(filepath=f"generator-{epoch + 1:04d}-shape-{str_image_shape}.hdf5")
+                model_dis.save(filepath=f"discriminator-{epoch + 1:04d}-shape-{str_image_shape}.hdf5")
 
-        # additional chief tasks during training
-        if is_chief:
-            status_message = f"batch_gen_loss={batch_gen_loss:.3f}, batch_dis_loss={batch_dis_loss:.3f}"
-            dataset.set_postfix_str(status_message)
-            logging.debug(status_message)
+            # update log files and tqdm status message
+            status_message = f"sec={epoch_duration:.3f}, gen_loss={gen_loss:.3f}, dis_loss={dis_loss:.3f}"
+            logging.info(f"finished epoch-{epoch + 1:04d} with {status_message}")
+            epochs.set_postfix_str(status_message)
 
-    return epoch_gen_loss, epoch_dis_loss, image_count
-
-
-@tf.function
-def discriminator_loss(tf_loss, real_output, fake_output):
-    real_loss = tf_loss(tf.ones_like(real_output), real_output)
-    fake_loss = tf_loss(tf.zeros_like(fake_output), fake_output)
-    total_loss = real_loss + fake_loss
-    return total_loss
-
-
-@tf.function
-def generator_loss(tf_loss, fake_output):
-    return tf_loss(tf.ones_like(fake_output), fake_output)
-
-
-@tf.function
-def train_step(generator, discriminator, optimizer_generator, optimizer_discriminator, image_batch, noise_dim, tf_loss):
-    # generate noise for projecting fake images
-    batch_size = tf.shape(image_batch)[0]
-    noise = tf.random.normal([batch_size, noise_dim])
-
-    # forward pass: inference through both models on tape, compute losses
-    with tf.GradientTape() as generator_tape, tf.GradientTape() as discriminator_tape:
-        fake_images = generator(noise, training=True)
-
-        real_image_guesses = discriminator(image_batch, training=True)
-        fake_image_guesses = discriminator(fake_images, training=True)
-
-        gen_loss = generator_loss(tf_loss, fake_output=fake_image_guesses)
-        disc_loss = discriminator_loss(tf_loss, real_output=real_image_guesses, fake_output=fake_image_guesses)
-
-    # collocate gradients from tapes
-    gradients_generator = generator_tape.gradient(gen_loss, generator.trainable_variables)
-    gradients_discriminator = discriminator_tape.gradient(disc_loss, discriminator.trainable_variables)
-    # backward pass: apply gradients via optimizers to update models
-    optimizer_generator.apply_gradients(zip(gradients_generator, generator.trainable_variables))
-    optimizer_discriminator.apply_gradients(zip(gradients_discriminator, discriminator.trainable_variables))
-    return gen_loss, disc_loss
+    # train loop
+    train_loop()
