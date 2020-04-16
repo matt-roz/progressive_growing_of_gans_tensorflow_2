2f04ec31e63fb058737b48d3defa3a02a7798eed
Matthias Rozanski <matthias.rozanski@rwth-aachen.de>
2020-04-15 22:25:19 -0700

    paper implementation

--- layers.py
+++ layers.py
@@ -1,4 +1,9 @@
+import logging
+
 import tensorflow as tf
+from tensorflow.python.ops import array_ops
+from tensorflow.python.keras import activations
+from tensorflow.python.framework import tensor_shape
 import numpy as np
 
 
@@ -46,6 +51,126 @@ class PixelNormalization(tf.keras.layers.Layer):
 
     def get_config(self):
         return {'epsilon': self._epsilon, 'data_format': self._data_format}
+
+
+class CustomDense(tf.keras.layers.Dense):
+    def __init__(self,
+                 input_shape,
+                 units,
+                 he_initializer_slope=1.0,
+                 use_bias=True,
+                 use_weight_scaling=True,
+                 activation=None,
+                 **kwargs):
+        if 'bias_initializer' in kwargs:
+            logging.warning(f"{self.__class__.__name__} ignores bias_initializer={kwargs['bias_initializer']}")
+            del kwargs['bias_initializer']
+        if 'kernel_initializer' in kwargs:
+            logging.warning(f"{self.__class__.__name__} ignores kernel_initializer={kwargs['kernel_initializer']}")
+            del kwargs['kernel_initializer']
+        super(CustomDense, self).__init__(units=units, use_bias=True, activation=None, **kwargs)
+        self.bias_initializer = tf.zeros_initializer()
+        self.he_initializer_slope = he_initializer_slope
+        self.use_weight_scaling = use_weight_scaling
+        self.wrapper_use_bias = use_bias
+        self.wrapper_activation = activations.get(activation)
+
+        # compute kernel initializer
+        input_shape = tensor_shape.TensorShape(input_shape)
+        last_dim = tensor_shape.dimension_value(input_shape[-1])
+        kernel_shape = (last_dim, self.units)
+        self.init_scale = he_initializer_scale(shape=kernel_shape, slope=self.he_initializer_slope)
+        self.post_scale = 1.0
+        if self.use_weight_scaling:
+            self.init_scale, self.post_scale = self.post_scale, self.init_scale
+        self.kernel_initializer = tf.random_normal_initializer(stddev=self.init_scale)
+
+    def build(self, input_shape):
+        super(CustomDense, self).build(input_shape)
+        self.use_bias = False
+
+    def call(self, inputs):
+        outputs = self.post_scale * super(CustomDense, self).call(inputs)
+
+        if self.wrapper_use_bias:
+            outputs = tf.nn.bias_add(outputs, self.bias)
+        if self.wrapper_activation is not None:
+            return self.wrapper_activation(outputs)
+        return outputs
+
+    def get_config(self):
+        base_config = super(CustomDense, self).get_config()
+        base_config['input_shape'] = self.input_shape
+        base_config['he_initializer_slope'] = self.he_initializer_slope
+        base_config['use_weight_scaling'] = self.use_weight_scaling
+        base_config['activation'] = activations.serialize(self.wrapper_activation)  # overrides base config
+        base_config['use_bias'] = self.wrapper_use_bias  # overrides base config
+        return base_config
+
+
+class CustomConv2D(tf.keras.layers.Conv2D):
+    def __init__(self,
+                 input_shape,
+                 filters,
+                 kernel_size,
+                 he_initializer_slope=1.0,
+                 use_bias=True,
+                 use_weight_scaling=True,
+                 activation=None,
+                 **kwargs):
+        if 'bias_initializer' in kwargs:
+            logging.warning(f"{self.__class__.__name__} ignores bias_initializer={kwargs['bias_initializer']}")
+            del kwargs['bias_initializer']
+        if 'kernel_initializer' in kwargs:
+            logging.warning(f"{self.__class__.__name__} ignores kernel_initializer={kwargs['kernel_initializer']}")
+            del kwargs['kernel_initializer']
+        super(CustomConv2D, self).__init__(filters=filters, kernel_size=kernel_size, use_bias=True, **kwargs)
+        self.bias_initializer = tf.zeros_initializer()
+        self.he_initializer_slope = he_initializer_slope
+        self.use_weight_scaling = use_weight_scaling
+        self.wrapper_activation = activations.get(activation)
+        self.wrapper_use_bias = use_bias
+
+        # compute kernel initializer
+        input_shape = tensor_shape.TensorShape(input_shape)
+        input_channel = self._get_input_channel(input_shape)
+        kernel_shape = self.kernel_size + (input_channel, self.filters)
+        self.init_scale = he_initializer_scale(shape=kernel_shape, slope=self.he_initializer_slope)
+        self.post_scale = 1.0
+        if self.use_weight_scaling:
+            self.init_scale, self.post_scale = self.post_scale, self.init_scale
+        self.kernel_initializer = tf.random_normal_initializer(stddev=self.init_scale)
+
+    def build(self, input_shape):
+        super(CustomConv2D, self).build(input_shape)
+        self.use_bias = False
+
+    def call(self, inputs, **kwargs):
+        outputs = self.post_scale * super(CustomConv2D, self).call(inputs)
+
+        if self.wrapper_use_bias:
+            if self.data_format == 'channels_first':
+                if self.rank == 1:
+                    # nn.bias_add does not accept a 1D input tensor.
+                    bias = array_ops.reshape(self.bias, (1, self.filters, 1))
+                    outputs += bias
+                else:
+                    outputs = tf.nn.bias_add(outputs, self.bias, data_format='NCHW')
+            else:
+                outputs = tf.nn.bias_add(outputs, self.bias, data_format='NHWC')
+
+        if self.wrapper_activation is not None:
+            return self.wrapper_activation(outputs)
+        return outputs
+
+    def get_config(self):
+        base_config = super(CustomConv2D, self).get_config()
+        base_config['input_shape'] = self.input_shape
+        base_config['he_initializer_slope'] = self.he_initializer_slope
+        base_config['use_weight_scaling'] = self.use_weight_scaling
+        base_config['activation'] = activations.serialize(self.wrapper_activation)  # overrides base config
+        base_config['use_bias'] = self.wrapper_use_bias  # overrides base config
+        return base_config
 
 
 class StandardDeviationLayer(tf.keras.layers.Layer):
--- model.py
+++ model.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 from tensorflow.keras.layers import Conv2D, LeakyReLU, Dense, Reshape, UpSampling2D, Flatten, BatchNormalization, \
     Conv2DTranspose
 
-from layers import PixelNormalization, DownSampling2D, StandardDeviationLayer, custom_conv2d, custom_dense
+from layers import PixelNormalization, DownSampling2D, StandardDeviationLayer, custom_conv2d, custom_dense, CustomConv2D, CustomDense
 
 
 def generator_example(
@@ -170,23 +170,32 @@ def generator_paper(
     def to_rgb(value: tf.Tensor, stage: int):
         # _x = Conv2D(filters=3, kernel_size=(1, 1), strides=(1, 1), use_bias=use_bias, kernel_initializer='he_normal',
         #            name=f'block_{stage}/toRGB')(value)
-        _x = custom_conv2d(x=value, filters=3, kernel_size=1, name=f'block_{stage}/toRGB', strides=(1, 1),
-                           use_weight_scaling=use_weight_scaling)
+        # _x = custom_conv2d(x=value, filters=3, kernel_size=1, name=f'block_{stage}/toRGB', strides=(1, 1),
+        #                   use_weight_scaling=use_weight_scaling)
+        _x = CustomConv2D(input_shape=(2**stage, 2**stage, num_features[stage]), filters=3, kernel_size=(1, 1),
+                          strides=(1, 1), use_weight_scaling=use_weight_scaling, use_bias=use_bias,
+                          name=f'block_{stage}/toRGB')(value)
         return _x
 
     def block(value: tf.Tensor, stage: int):
         # _x = Conv2D(filters=num_features[stage], kernel_size=(3, 3), strides=(1, 1), padding='same',
         #            use_bias=use_bias, kernel_initializer='he_normal', name=f'block_{stage}/conv2d_1')(value)
-        _x = custom_conv2d(x=value, filters=num_features[stage], kernel_size=3, strides=(1, 1),
-                           name=f'block_{stage}/conv2d_1', he_initializer_slope=0.0,
-                           use_weight_scaling=use_weight_scaling)
+        # _x = custom_conv2d(x=value, filters=num_features[stage], kernel_size=3, strides=(1, 1),
+        #                   name=f'block_{stage}/conv2d_1', he_initializer_slope=0.0,
+        #                   use_weight_scaling=use_weight_scaling)
+        _x = CustomConv2D(input_shape=value.shape[1:], filters=num_features[stage], kernel_size=(3, 3), strides=(1, 1),
+                          padding='same', he_initializer_slope=0.0, use_weight_scaling=use_weight_scaling,
+                          use_bias=use_bias, name=f'block_{stage}/conv2d_1')(value)
         _x = LeakyReLU(alpha=leaky_alpha, name=f'block_{stage}/activation_1')(_x)
         _x = PixelNormalization(name=f'block_{stage}/pixel_norm_1')(_x)
         # _x = Conv2D(filters=num_features[stage], kernel_size=(3, 3), strides=(1, 1), padding='same',
         #             use_bias=use_bias, kernel_initializer='he_normal', name=f'block_{stage}/conv2d_2')(_x)
-        _x = custom_conv2d(x=_x, filters=num_features[stage], kernel_size=3, strides=(1, 1),
-                           name=f'block_{stage}/conv2d_2', he_initializer_slope=0.0,
-                           use_weight_scaling=use_weight_scaling)
+        # _x = custom_conv2d(x=_x, filters=num_features[stage], kernel_size=3, strides=(1, 1),
+        #                   name=f'block_{stage}/conv2d_2', he_initializer_slope=0.0,
+        #                   use_weight_scaling=use_weight_scaling)
+        _x = CustomConv2D(input_shape=_x.shape[1:], filters=num_features[stage], kernel_size=(3, 3), strides=(1, 1),
+                          padding='same', he_initializer_slope=0.0, use_weight_scaling=use_weight_scaling,
+                          use_bias=use_bias, name=f'block_{stage}/conv2d_2')(value)
         _x = LeakyReLU(alpha=leaky_alpha, name=f'block_{stage}/activation_2')(_x)
         _x = PixelNormalization(name=f'block_{stage}/pixel_norm_2')(_x)
         return _x
@@ -198,18 +207,23 @@ def generator_paper(
 
     # project from noise to minimum features, apply block 2 to features
     _target_shape = (4, 4, num_features[2])
-    _units = np.prod(_target_shape)
+    _units = int(np.prod(_target_shape))
     # features = Dense(units=_units, use_bias=use_bias, kernel_initializer='he_normal', input_shape=input_shape,
     #                 name='block_2/dense_projector')(x)
-    features = custom_dense(x=x, units=_units, use_weight_scaling=use_weight_scaling, name='block_2/dense_projector')
+    # features = custom_dense(x=x, units=_units, use_weight_scaling=use_weight_scaling, name='block_2/dense_projector')
+    features = CustomDense(input_shape=x.shape, units=_units, use_weight_scaling=use_weight_scaling, use_bias=use_bias,
+                           name='block_2/dense_projector')(x)
     features = Reshape(target_shape=_target_shape, input_shape=(_units,), name='block_2/feature_reshape')(features)
     features = LeakyReLU(alpha=leaky_alpha, name='block_2/activation_1')(features)
     features = PixelNormalization(name='block_2/pixel_norm_1')(features)
     # features = Conv2D(filters=num_features[2], kernel_size=(3, 3), strides=(1, 1), padding='same',
     #                  use_bias=use_bias, kernel_initializer='he_normal', name='block_2/conv2d_1')(features)
-    features = custom_conv2d(x=features, filters=num_features[2], kernel_size=3, strides=(1, 1),
-                             name=f'block_2/conv2d_1', he_initializer_slope=0.0,
-                             use_weight_scaling=use_weight_scaling)
+    # features = custom_conv2d(x=features, filters=num_features[2], kernel_size=3, strides=(1, 1),
+    #                         name=f'block_2/conv2d_1', he_initializer_slope=0.0,
+    #                         use_weight_scaling=use_weight_scaling)
+    features = CustomConv2D(input_shape=_target_shape, filters=num_features[2], kernel_size=(3, 3), strides=(1, 1),
+                            padding='same', he_initializer_slope=0.0, use_weight_scaling=use_weight_scaling,
+                            use_bias=use_bias, name=f'block_2/conv2d_1')(features)
     features = LeakyReLU(alpha=leaky_alpha, name='block_2/activation_2')(features)
     features = PixelNormalization(name='block_2/pixel_norm_2')(features)
     image_out = to_rgb(value=features, stage=2)
@@ -260,24 +274,34 @@ def discriminator_paper(
     def from_rgb(value: tf.Tensor, stage: int):
         # _x = Conv2D(filters=num_features[stage], kernel_size=(1, 1), strides=(1, 1), use_bias=use_bias,
         #            kernel_initializer='he_normal', name=f'block_{stage}/fromRGB')(value)
-        _x = custom_conv2d(x=value, filters=num_features[stage], kernel_size=1, strides=(1, 1),
-                           he_initializer_slope=0.0, name=f'block_{stage}/fromRGB',
-                           use_weight_scaling=use_weight_scaling)
+        # _x = custom_conv2d(x=value, filters=num_features[stage], kernel_size=1, strides=(1, 1),
+        #                   he_initializer_slope=0.0, name=f'block_{stage}/fromRGB',
+        #                   use_weight_scaling=use_weight_scaling)
+        _x = CustomConv2D(input_shape=value.shape[1:], filters=num_features[stage], kernel_size=(3, 3), strides=(1, 1),
+                          padding='same', he_initializer_slope=0.0, use_weight_scaling=use_weight_scaling,
+                          use_bias=use_bias, name=f'block_{stage}/fromRGB')(value)
         _x = LeakyReLU(alpha=leaky_alpha, name=f'block_{stage}/activation_rgb')(_x)
         return _x
 
     def block(value: tf.Tensor, stage: int):
         # _x = Conv2D(filters=num_features[stage], kernel_size=(3, 3), strides=(1, 1), padding='same',
         #            use_bias=use_bias, kernel_initializer='he_normal', name=f'block_{stage}/conv2d_1')(value)
-        _x = custom_conv2d(x=value, filters=num_features[stage], kernel_size=3, strides=(1, 1),
-                           he_initializer_slope=0.0, name=f'block_{stage}/conv2d_1',
-                           use_weight_scaling=use_weight_scaling)
+        # _x = custom_conv2d(x=value, filters=num_features[stage], kernel_size=3, strides=(1, 1),
+        #                   he_initializer_slope=0.0, name=f'block_{stage}/conv2d_1',
+        #                   use_weight_scaling=use_weight_scaling)
+        _x = CustomConv2D(input_shape=value.shape[1:], filters=num_features[stage], kernel_size=(3, 3), strides=(1, 1),
+                          padding='same', he_initializer_slope=0.0, use_weight_scaling=use_weight_scaling,
+                          use_bias=use_bias, name=f'block_{stage}/conv2d_1')(value)
         _x = LeakyReLU(alpha=leaky_alpha, name=f'block_{stage}/activation_1')(_x)
         # _x = Conv2D(filters=num_features[stage - 1], kernel_size=(3, 3), strides=(1, 1), padding='same',
         #            use_bias=use_bias, kernel_initializer='he_normal', name=f'block_{stage}/conv2d_2')(_x)
-        _x = custom_conv2d(x=_x, filters=num_features[stage - 1], kernel_size=3, strides=(1, 1),
-                           he_initializer_slope=0.0, name=f'block_{stage}/conv2d_2',
-                           use_weight_scaling=use_weight_scaling)
+        # _x = custom_conv2d(x=_x, filters=num_features[stage - 1], kernel_size=3, strides=(1, 1),
+        #                   he_initializer_slope=0.0, name=f'block_{stage}/conv2d_2',
+        #                   use_weight_scaling=use_weight_scaling)
+        _x = CustomConv2D(input_shape=value.shape[1:], filters=num_features[stage - 1], kernel_size=(3, 3),
+                          strides=(1, 1), padding='same', he_initializer_slope=0.0,
+                          use_weight_scaling=use_weight_scaling, use_bias=use_bias,
+                          name=f'block_{stage}/conv2d_2')(value)
         _x = LeakyReLU(alpha=leaky_alpha, name=f'block_{stage}/activation_2')(_x)
         return _x
 
@@ -304,14 +328,21 @@ def discriminator_paper(
     x = StandardDeviationLayer(name=f'block_2/stddev_layer')(features)
     # x = Conv2D(filters=num_features[2], kernel_size=(3, 3), strides=(1, 1), padding='same', use_bias=use_bias,
     #           kernel_initializer='he_normal', name=f'block_2/conv2d_1')(x)
-    x = custom_conv2d(x=x, filters=num_features[2], kernel_size=3, strides=(1, 1),
-                      name=f'block_2/conv2d_1', use_weight_scaling=use_weight_scaling)
+    # x = custom_conv2d(x=x, filters=num_features[2], kernel_size=3, strides=(1, 1),
+    #                  name=f'block_2/conv2d_1', use_weight_scaling=use_weight_scaling)
+    x = CustomConv2D(input_shape=x.shape[1:], filters=num_features[2], kernel_size=(3, 3), strides=(1, 1),
+                     padding='same', he_initializer_slope=0.0, use_weight_scaling=use_weight_scaling, use_bias=use_bias,
+                     name=f'block_2/conv2d_1')(x)
     x = LeakyReLU(alpha=leaky_alpha, name=f'block_2/activation_1')(x)
     x = Flatten(name='block_2/flatten')(x)
     # x = Dense(units=512, use_bias=use_bias, kernel_initializer='he_normal', name='block_2/dense_1')(x)
-    x = custom_dense(x=x, units=512,  name='block_2/dense_1', use_weight_scaling=use_weight_scaling)
+    # x = custom_dense(x=x, units=512,  name='block_2/dense_1', use_weight_scaling=use_weight_scaling)
+    x = CustomDense(input_shape=x.shape[1:], units=512, he_initializer_slope=1.0, use_bias=use_bias,
+                    use_weight_scaling=use_weight_scaling, name='block_2/dense_1')(x)
     x = LeakyReLU(alpha=leaky_alpha, name=f'block_2/activation_2')(x)
     # x = Dense(units=1, use_bias=use_bias, kernel_initializer='he_normal', activation='linear', name='block_2/dense_2')(x)
-    x = custom_dense(x=x, units=1, name='block_2/dense_2', use_weight_scaling=use_weight_scaling)
+    # x = custom_dense(x=x, units=1, name='block_2/dense_2', use_weight_scaling=use_weight_scaling)
+    x = CustomDense(input_shape=x.shape[1:], units=1, he_initializer_slope=1.0, use_bias=use_bias,
+                    use_weight_scaling=use_weight_scaling, name='block_2/dense_2')(x)
 
     return tf.keras.models.Model(inputs=[inputs, alpha], outputs=x, name=name)
