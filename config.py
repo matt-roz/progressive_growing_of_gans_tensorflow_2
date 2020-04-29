092c48b7b8319976961693ffa9d4b9bcadb7c749
Matthias Rozanski <matthias.rozanski@rwth-aachen.de>
2020-04-28 17:31:00 -0700

    cleaned up

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ import platform
 from datetime import datetime
 
 import tensorflow as tf
+from data import celeb_a_hq_process_func
 
 # ----------------------------------------------------------------------------------------------------------------------
 # Convenience class that behaves exactly like dict(), but allows accessing
@@ -35,16 +36,16 @@ general_config.log_dir                  = os.path.join('/media', 'storage', 'out
 general_config.data_dir                 = os.path.join('/media', 'storage', 'tensorflow_datasets')
 general_config.XLA                      = False     # bool: de-/activates XLA JIT compilation
 general_config.strategy                 = 'default' # str: distribution strategy; options are ['default', 'mirrored', 'multimirrored']
-general_config.checkpoint_freq          = 27        # uint: frequency to checkpoint models with (0 = disabled)
-general_config.eval_freq                = 1         # uint: frequency to evaluate models with (0 = disabled)
-general_config.log_freq                 = 'epoch'   # str: frequency to log with ('' = disabled); options are ['batch', 'epoch', '']
+general_config.checkpoint_freq          = 27        # uint: epoch frequency to checkpoint models with (0 = disabled)
+general_config.eval_freq                = 1         # uint: epoch frequency to evaluate models with (0 = disabled)
+general_config.log_freq                 = 1         # uint: epoch frequency to log with (0 = disabled)
 
 model_config = EasyDict()               # configuration of model building
 model_config.leaky_alpha                = 0.3       # float: leakiness of LeakyReLU activations
 model_config.generator_ema              = 0.999     # float: exponential moving average of final_generator
 model_config.resolution                 = 256       # uint: final resolution in [4, 8, 16, 32, 64, 128, 256, 512, 1024]
 model_config.noise_dim                  = 512       # uint: noise_dim generator projects from
-model_config.epsilon                    = 1e-8      # float: small constant for numerical stability in PixelNormalization as well as StandardDeviation layers
+model_config.epsilon                    = 1e-8      # float: small constant for numerical stability in PixelNormalization as well as StandardDeviation Layer
 model_config.use_bias                   = True      # bool: de-/activates usage of biases in all trainable layers
 model_config.use_stages                 = True      # bool: de-/activates progressive training of model in stages (if deactivated only last stage for final resolution is trained)
 model_config.use_fused_scaling          = True      # bool: de-/activates up- and downsampling of images via strides=(2, 2) in Conv2D and Conv2DTranspose (else UpSampling2D and AveragePooling2D is used)
@@ -53,7 +54,7 @@ model_config.use_alpha_smoothing        = True      # bool: de-/activates smooth
 model_config.use_noise_normalization    = True      # bool: de-/activates pixel_normalization on noise input at generator start
 
 train_config = EasyDict()               # configuration of train parameters
-train_config.epochs                     = 540       # uint: number of epochs to train for
+train_config.epochs                     = 432       # uint: number of epochs to train for
 train_config.epochs_per_stage           = 54        # uint: number of epochs per stage; alpha is increased linearly from alpha_init to 1.0 in halfway through
 train_config.alpha_init                 = 0.0       # float: initial alpha value to smooth in images from previous block after stage in model has been increased
 train_config.use_epsilon_penalty        = True      # bool: de-/activates epsilon_drift_penalty applied to discriminator loss as described in https://arxiv.org/abs/1710.10196
@@ -65,12 +66,14 @@ train_config.wgan_target                = 1.0       # float: target scalar for g
 data_config = EasyDict()                # configuration of data set pipeline
 data_config.registered_name             = 'celeb_a_hq'                   # str: name argument for tensorflow_datasets.load
 data_config.split                       = 'train'                        # str: split argument for tensorflow_datasets.load
+data_config.num_examples                = 30000                          # uint: number of examples train dataset will contain according to loaded split
 data_config.caching                     = False                          # bool: de-/activates dataset caching to file or system memory (see cache_file)
 data_config.cache_file                  = os.path.join('/tmp', f'{timestamp}-tf-dataset.cache')  # str: ignored if caching is false, else location of temporary cache_file ("" = load entire dataset into system memory)
+data_config.process_func                = celeb_a_hq_process_func        # callable: function to process each dataset entry with
 data_config.map_parallel_calls          = tf.data.experimental.AUTOTUNE  # int: number of parallel entries to apply 'process_functions' asynchronously
 data_config.prefetch_parallel_calls     = tf.data.experimental.AUTOTUNE  # int: number of parallel threads to prefetch entries with concurrently
 data_config.interleave_parallel_calls   = tf.data.experimental.AUTOTUNE  # int: number of parallel threads to access dataset shards/files
-data_config.replica_batch_sizes         = {2: 16, 3: 16, 4: 16, 5: 16, 6: 16, 7: 16, 8: 14, 9: 6, 10: 3}  # dict: batch_size at stage
+data_config.replica_batch_sizes         = {2: 512, 3: 378, 4: 256, 5: 128, 6: 64, 7: 32, 8: 14, 9: 6, 10: 3}             # dict: batch_size at stage
 data_config.buffer_sizes                = {2: 5000, 3: 5000, 4: 2500, 5: 1250, 6: 500, 7: 400, 8: 300, 9: 250, 10: 200}  # dict: buffer_size at stage
 
 optimizer_config = EasyDict()           # configuration of adam optimizers for both generator and discriminator
@@ -96,6 +99,9 @@ conf.data                               = data_config
 conf.optimizer                          = optimizer_config
 conf.log                                = log_config
 
+# uncomment next line to apply full training according to the original contribution: https://arxiv.org/abs/1710.10196
+# data_config.replica_batch_sizes = {2: 16, 3: 16, 4: 16, 5: 16, 6: 16, 7: 16, 8: 14, 9: 6, 10: 3}; model_config.resolution = 1024; train_config.epochs = 540
+
 # ----------------------------------------------------------------------------------------------------------------------
 # Placeholders (these configurations are automatically set at runtime)
 # ----------------------------------------------------------------------------------------------------------------------
@@ -110,3 +116,5 @@ general_config.summary                  = None  # tf.FileWriter: FileWriter inst
 data_config.data_dir                    = None  # str: path to data_dir (same as general_config.data_dir)
 model_config.final_stage                = None  # uint: final model stage
 model_config.alpha                      = None  # uint: current alpha for smoothing
+model_config.alpha_step                 = None  # float: delta-alpha increase per image during training
+
