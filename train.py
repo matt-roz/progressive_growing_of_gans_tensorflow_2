6a27ecc5ec6cac6d40896017f4eb5999ad8432f4
Matthias Rozanski <matthias.rozanski@rwth-aachen.de>
2020-05-09 23:45:38 -0700

    added doc

--- train.py
+++ train.py
@@ -9,6 +9,7 @@ import numpy as np
 import tensorflow as tf
 from tqdm import tqdm
 from tensorflow.keras.utils import plot_model
+from tensorflow.python.framework.composite_tensor import CompositeTensor
 
 from config import conf
 from data import get_dataset_pipeline
@@ -17,6 +18,14 @@ from losses import wasserstein_discriminator_loss, wasserstein_generator_loss, d
     wasserstein_gradient_penalty
 from utils import save_eval_images, transfer_weights
 
+"""
+global variables for 'handier' access to changing objects references. Currently, while not pretty, this is the best way 
+to deal with potential side-effects with graph mode execution via compiled tf.functions. For Progressive GANs object 
+references for keras objects are updated, since models are growing progressively - graph execution does not support 
+dynamically growing models out-of-the box. Functions, which are invoking member functions of newly created models, must 
+be retraced and recompiled via AutoGraph. In this case, when a new stage is reached, train_step_fn is updated to the 
+mentioned retraced graph execution function (retracing all underlying function invocations such as model.__call__).
+"""
 generator = None
 final_gen = None
 discriminator = None
@@ -26,17 +35,20 @@ global_batch_size = None
 train_step_fn = None
 
 
-def replica_train_step(batch: tf.Tensor, alpha: tf.Tensor) -> Tuple[tf.Tensor, tf.Tensor]:
-    """Trains Progressive GAN for one batch.
+def replica_train_step(batch: tf.Tensor, alpha: tf.Tensor) -> tf.Tensor:
+    """Trains Progressive GAN for one batch in replica context. Loss functions used in this context must return per
+     examples losses and must be scaled to global_batch_size.
 
     Args:
-        batch: a 4D-Tensor containing the images to train with
+        batch: a 4D-Tensor containing the images to train with. First dimension depicts per replica batch_size.
         alpha: a scalar depicting the current alpha for smoothing images of Progressive GANs
 
     Returns:
-        A tuple of length two containing batch train information:
-            gen_loss (float): mean generator loss
-            disc_loss (tf.Tensor): mean discriminator loss shape=(3,) with wasserstein_loss, gradient_loss, epsilon_loss
+        A 1D-Tensor with shape=(4,) containing train information of local replica:
+            gen_loss: wasserstein generator loss on replica
+            disc_loss: wasserstein discriminator loss on replica
+            disc_gradient_loss: gradient penalty loss of discriminator on replica
+            disc_epsilon_loss: epsilon drift loss of discriminator on replica
     """
     # generate noise for projecting fake images
     replica_batch_size = tf.shape(batch)[0]
@@ -77,39 +89,45 @@ def replica_train_step(batch: tf.Tensor, alpha: tf.Tensor) -> Tuple[tf.Tensor, t
     return tf.stack((gen_loss, _disc_loss, disc_gradient_loss, disc_epsilon_loss))
 
 
-def node_train_step(batch: tf.Tensor, alpha: tf.Tensor) -> Tuple[tf.Tensor, tf.Tensor]:
+def global_train_step(batch: Tuple[tf.Tensor, CompositeTensor], alpha: tf.Tensor) -> Tuple[tf.Tensor, tf.Tensor]:
+    """Trains Progressive GAN for one batch over all replicas. Losses are reduced over all replicas with reduce_sum.
+    This function expects all replica losses to be scaled with global_batch_size.
+
+    Args:
+        batch: Either a 4D-Tensor for single-replica training or a CompositeTensor for multi-replica training from a
+            distributed dataset
+        alpha: a scalar depicting the current alpha for smoothing images of Progressive GANs
+
+    Returns:
+        A 1D-Tensor with shape=(4,) containing train information:
+            gen_loss: wasserstein generator loss for batch over all replicas
+            disc_loss: wasserstein discriminator loss for batch over all replicas
+            disc_gradient_loss: gradient penalty loss of discriminator for batch over all replicas
+            disc_epsilon_loss: epsilon drift loss of discriminator for batch over all replicas
+    """
+    # log whenever this function is traced into a graph function (after each stage increase of Progressive GANs)
     if not conf.general.train_eagerly:
         logging.info(f'tf.function tracing train_step: batch={batch}, alpha={alpha}')
     per_replica_losses = conf.general.strategy.experimental_run_v2(replica_train_step, args=(batch, alpha))
-    return conf.general.strategy.reduce(tf.distribute.ReduceOp.SUM, per_replica_losses, axis=None)
+    reduced_losses = conf.general.strategy.reduce(tf.distribute.ReduceOp.SUM, per_replica_losses, axis=None)
+    return reduced_losses
 
 
-def epoch_step(
-        # generator: tf.keras.Model,
-        # final_generator: tf.keras.Model,
-        # discriminator: tf.keras.Model,
-        dataset: tf.data.Dataset,
-        # optimizer_gen: tf.optimizers.Optimizer,
-        # optimizer_dis: tf.optimizers.Optimizer,
-        current_epoch: int,
-        num_steps: int) -> Tuple[float, tf.Tensor, float]:
-    """Trains Progressive GAN for one epoch.
+def epoch_step(dataset: tf.data.Dataset, current_epoch: int, num_steps: int) -> Tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
+    """Trains Progressive GAN for one epoch. This function is always executed eagerly and can not be compiled into a
+    graph function due to transfer_weights having to be executed within a python thread to keep an exponential moving
+    average of the generator.
     
     Args:
-        generator: generator model of the GAN
-        final_generator: exponential moving average of generator across all progressive stages
-        discriminator: discriminator model of the GAN
         dataset: dataset to train one epoch on
-        optimizer_gen: optimizer to train generator with
-        optimizer_dis: optimizer to train discriminator with
-        current_epoch: current epoch
+        current_epoch: current epoch number
         num_steps: number of steps (batches to train for) in current epoch
 
     Returns:
         A tuple of length three containing epoch train information:
-            gen_loss (float): mean generator loss
-            disc_loss (tf.Tensor): mean discriminator loss shape=(3,) with wasserstein_loss, gradient_loss, epsilon_loss
-            image_count (float): number of images GAN was trained with in epoch
+            gen_loss: mean generator loss over all replicas
+            disc_loss: mean discriminator loss shape=(3,) with wasserstein_loss, gradient_loss, epsilon_loss
+            image_count: number of images Progressive GAN was trained with in epoch
     """
     # return metrics
     _epoch_gen_loss, _epoch_dis_loss, _image_count = 0.0, 0.0, 0.0
@@ -162,9 +180,6 @@ def instantiate_stage_objects(stage: int) -> \
     global_batch_size = conf.data.replica_batch_sizes[stage] * conf.general.strategy.num_replicas_in_sync
     dataset = get_dataset_pipeline(name=f"{conf.data.registered_name}/{2**stage}", batch_size=global_batch_size,
                                    buffer_size=conf.data.buffer_sizes[stage], **conf.data)
-    # options = tf.data.Options()
-    # options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.OFF
-    # dataset = dataset.with_options(options)
     dataset = conf.general.strategy.experimental_distribute_dataset(dataset)
 
     # create models
@@ -175,7 +190,7 @@ def instantiate_stage_objects(stage: int) -> \
     logging.info(f"Successfully instantiated {dis.name} and {gen.name} for stage={stage}")
     gen.summary(print_fn=logging.info, line_length=150, positions=[.33, .55, .67, 1.])
     dis.summary(print_fn=logging.info, line_length=150, positions=[.33, .55, .67, 1.])
-    if conf.general.save:
+    if conf.general.is_chief and conf.general.save:
         plot_model(gen, os.path.join(conf.general.out_dir, f"net_{gen.name}.png"), True, False, dpi=178)
         plot_model(dis, os.path.join(conf.general.out_dir, f"net_{dis.name}.png"), True, False, dpi=178)
     return gen, dis, dataset, optimizer_gen, optimizer_dis
@@ -183,15 +198,13 @@ def instantiate_stage_objects(stage: int) -> \
 
 def train():
     global optimizer_gen, optimizer_dis, global_batch_size, final_gen, generator, discriminator, train_step_fn
-    # set optimizer settings
-    # tf.config.optimizer.set_jit(conf.general.XLA)
 
     # instantiate target model that will be an exponential moving average of generator, log summary
     with conf.general.strategy.scope():
         final_gen = generator_paper(stop_stage=conf.model.final_stage, return_all_outputs=True, **conf.model)
     logging.info(f"Successfully instantiated the following final model {final_gen.name}")
     final_gen.summary(print_fn=logging.info, line_length=150, positions=[.33, .55, .67, 1.])
-    if conf.general.save:
+    if conf.general.is_chief and conf.general.save:
         plot_model(final_gen, os.path.join(conf.general.out_dir, f"net_{final_gen.name}.png"), True, False, dpi=178)
 
     # instantiate initial stage trainable models, optimizers and dataset
@@ -199,44 +212,43 @@ def train():
     with conf.general.strategy.scope():
         generator, discriminator, train_dataset, optimizer_gen, optimizer_dis = instantiate_stage_objects(current_stage)
     transfer_weights(source_model=generator, target_model=final_gen, beta=0.0)  # force same initialization
+
+    # extract image_shape from train_dataset's element_spec (which is depending on distribution strategy)
     if isinstance(train_dataset.element_spec, tf.TensorSpec):
         image_shape = train_dataset.element_spec.shape[1:]
     elif isinstance(train_dataset.element_spec, tf.python.distribute.values.PerReplicaSpec):
         image_shape = train_dataset.element_spec._value_specs[0].shape[1:]
     else:
-        raise RuntimeError()
-
-    # epoch iterator
-    epochs = tqdm(range(conf.train.epochs), f"Progressive-GAN(stage={current_stage}, shape={image_shape}", unit='epoch')
-    logging.info(f"Starting to train Stage {current_stage}")
+        raise RuntimeError(f"dataset {train_dataset} returns unknown element_spec of type {type(train_dataset.element_spec)}")
 
     # variables, counters, timings, etc.
     total_image_count = 0
     replica_batch_size = conf.data.replica_batch_sizes[current_stage]
     global_batch_size = replica_batch_size * conf.general.strategy.num_replicas_in_sync
-    steps_per_epoch = int(conf.data.num_examples // global_batch_size) + 1
+    steps_per_epoch = int(conf.data.num_examples // global_batch_size)
     stage_start_time = train_start_time = time.time()
     random_noise = tf.random.normal(shape=(16, conf.model.noise_dim), seed=1000)
 
-    # compile tf.function
-    # input_signature = [tf.TensorSpec(shape=train_dataset.element_spec.shape, dtype=tf.float32), tf.TensorSpec(shape=tuple(), dtype=tf.float32)]
-    train_step_fn = node_train_step if conf.general.train_eagerly else tf.function(node_train_step, experimental_compile=conf.general.XLA)
+    # create epoch iterator, compile train_step_fn depending on run mode configuration
+    epochs = tqdm(range(conf.train.epochs), f"Progressive-GAN(stage={current_stage}, shape={image_shape}", unit='epoch')
+    logging.info(f"Starting to train Stage {current_stage}")
+    train_step_fn = global_train_step if conf.general.train_eagerly else tf.function(global_train_step, experimental_compile=conf.general.XLA)
 
     for epoch in epochs:
         # make an epoch step
         epoch_start_time = time.time()
         gen_loss, dis_loss, image_count = epoch_step(train_dataset, epoch, steps_per_epoch)
         epoch_duration = time.time() - epoch_start_time
-        total_image_count += int(image_count)
+        total_image_count += int(image_count.numpy())
 
-        # TensorBoard logging
+        # chief does TensorBoard logging
         if conf.general.is_chief and conf.general.logging and conf.general.log_freq and (epoch + 1) % conf.general.log_freq == 0:
             with conf.general.summary.as_default():
                 num_replicas = conf.general.strategy.num_replicas_in_sync
                 num_replicas_per_node = num_replicas / conf.general.nnodes
                 batches_per_second = tf.cast(steps_per_epoch, tf.float32) / epoch_duration
-                images_per_second = image_count/epoch_duration
-                seconds_per_kimages = 1000*epoch_duration/image_count
+                images_per_second = image_count.numpy()/epoch_duration
+                seconds_per_kimages = 1000*epoch_duration/image_count.numpy()
                 disc_gradient_penalty = dis_loss[1] * (conf.train.wgan_target ** 2) / conf.train.wgan_lambda
                 disc_gradient_mixed_norm = np.sqrt(disc_gradient_penalty + 1e-8) + conf.train.wgan_target
                 tf.summary.scalar("train_speed/epoch", epoch, epoch)
@@ -270,13 +282,13 @@ def train():
                 tf.summary.scalar("optimizers/epoch/discriminator_learning_rate", optimizer_dis.lr, epoch)
                 tf.summary.scalar("optimizers/epoch/generator_learning_rate", optimizer_gen.lr, epoch)
 
-        # save eval images
+        # chief saves eval images
         if conf.general.is_chief and conf.general.evaluate and conf.general.eval_freq and (epoch + 1) % conf.general.eval_freq == 0:
-            n = np.minimum(replica_batch_size, len(random_noise))
+            n = np.minimum(replica_batch_size, len(random_noise))  # else some GPUs might OOM for large resolutions
             save_eval_images(random_noise[:n], generator, epoch, conf.general.out_dir, tf.constant(conf.model.alpha))
             save_eval_images(random_noise[:n], final_gen, epoch, conf.general.out_dir, tf.constant(1.0), current_stage)
 
-        # save model checkpoints
+        # chief saves model checkpoints
         if conf.general.is_chief and conf.general.save and conf.general.checkpoint_freq and (epoch + 1) % conf.general.checkpoint_freq == 0:
             s = 'x'.join([str(x) for x in image_shape])
             gen_file = os.path.join(conf.general.out_dir, f"cp_{generator.name}_epoch-{epoch+1:04d}_shape-{s}.h5")
@@ -286,13 +298,7 @@ def train():
             discriminator.save(filepath=dis_file)
             final_gen.save(filepath=fin_file)
 
-        # update log files and tqdm status message
-        _str_duration = str(timedelta(seconds=epoch_duration))
-        status_message = f"duration={_str_duration}, gen_loss={gen_loss:.3f}, dis_loss={tf.reduce_sum(dis_loss):.3f}"
-        logging.info(f"Finished epoch-{epoch+1:04d} with {status_message}")
-        epochs.set_postfix_str(status_message)
-
-        # check stage increase
+        # all workers check for stage increase
         if (epoch + 1) % conf.train.epochs_per_stage == 0 and current_stage < conf.model.final_stage:
             # increment stages, log progress
             current_stage += 1
@@ -305,12 +311,14 @@ def train():
             # get dataset pipeline for new images, instantiate next stage models, get new optimizers
             with conf.general.strategy.scope():
                 _gen, _dis, train_dataset, optimizer_gen, optimizer_dis = instantiate_stage_objects(current_stage)
+
+            # extract image_shape from train_dataset's element_spec (which is depending on distribution strategy)
             if isinstance(train_dataset.element_spec, tf.TensorSpec):
                 image_shape = train_dataset.element_spec.shape[1:]
             elif isinstance(train_dataset.element_spec, tf.python.distribute.values.PerReplicaSpec):
                 image_shape = train_dataset.element_spec._value_specs[0].shape[1:]
             else:
-                raise RuntimeError()
+                raise RuntimeError(f"dataset {train_dataset} returns unknown element_spec of type {type(train_dataset.element_spec)}")
 
             # transfer weights from previous stage models to current_stage models
             transfer_weights(source_model=generator, target_model=_gen, beta=0.0)
@@ -323,17 +331,22 @@ def train():
             generator = _gen
             discriminator = _dis
 
-            # compile tf.function
-            # input_signature = [tf.TensorSpec(shape=train_dataset.element_spec.shape, dtype=tf.float32), tf.TensorSpec(shape=tuple(), dtype=tf.float32)]
-            train_step_fn = node_train_step if conf.general.train_eagerly else tf.function(node_train_step, experimental_compile=conf.general.XLA)
+            # compile train_step_fn depending on run mode configuration
+            train_step_fn = global_train_step if conf.general.train_eagerly else tf.function(global_train_step, experimental_compile=conf.general.XLA)
 
             # update variables, counters/tqdm postfix
             replica_batch_size = conf.data.replica_batch_sizes[current_stage]
             global_batch_size = replica_batch_size * conf.general.strategy.num_replicas_in_sync
-            steps_per_epoch = int(conf.data.num_examples // global_batch_size) + 1
+            steps_per_epoch = int(conf.data.num_examples // global_batch_size)
             conf.model.alpha = conf.train.alpha_init
             epochs.set_description_str(f"Progressive-GAN(stage={current_stage}, shape={image_shape}")
             logging.info(f"Starting to train Stage {current_stage}")
+
+        # update log files and tqdm status message
+        _str_duration = str(timedelta(seconds=epoch_duration))
+        status_message = f"duration={_str_duration}, gen_loss={gen_loss:.3f}, dis_loss={tf.reduce_sum(dis_loss):.3f}"
+        logging.info(f"Finished epoch-{epoch+1:04d} with {status_message}")
+        epochs.set_postfix_str(status_message)
 
     # final
     current_time = time.time()
